e2babfdbecb1f6f3e82611b58ea931ae596ffa64
vbabai <flash.slava@gmail.com>
2019-02-09 14:59:22 +0200

    added filters

---
+++ WebAPITest/Babai.Filters/Filters/ResourceFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Babai.Filters.Filters
+{
+    public class ResourceFilter : IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            
+        }
+    }
+}
